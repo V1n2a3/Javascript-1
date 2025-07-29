c72b739531669e3feea984d4869d94a6a72718f9
V1n2a3 <101231939+V1n2a3@users.noreply.github.com>
2025-07-29 12:18:31 +0000

    conversion

--- file1/03_conversionOperations.js
+++ file1/03_conversionOperations.js
@@ -16,8 +16,22 @@ let booleanisLoggedIn = Boolean(isLoggedIn)
 let someNumber = 45
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof(stringNumber));
+// console.log(stringNumber)
+// console.log(typeof(stringNumber));
+
+
+//*************Operations************/
+
+let value = 45
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Wassup"
+let str2 = " TempestWolf"
+
+let str3 = str1 + str2
+
+console.log(str3);
 
 
 
