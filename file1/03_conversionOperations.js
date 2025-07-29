a478c96f0916e8f9bc2f198afa3a3a3fe4d8dfea
V1n2a3 <101231939+V1n2a3@users.noreply.github.com>
2025-07-29 09:23:33 +0000

    datatype conversion

---
+++ file1/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueinNumber = Number(score)
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
+
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+let someNumber = 45
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber));
+
+
+
+
+
+
+
+
+
