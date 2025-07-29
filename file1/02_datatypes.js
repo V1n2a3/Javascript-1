c5c7d49cb4bf6ba223b4e6c75868c838f77991b5
V1n2a3 <101231939+V1n2a3@users.noreply.github.com>
2025-07-29 07:56:25 +0000

    datatypes

--- file1/02_datatypes.js
+++ file1/02_datatypes.js
@@ -2,3 +2,14 @@
 
 // alert(3 + 3) // we are using nodejs , not browser
 
+console.log(3 + 3)
+
+console.log("Vinay")
+
+let name ="Vinay"
+let age = 23
+let state = "Latur"
+
+console.log(typeof "Vinay");
+console.log(typeof 23)
+
