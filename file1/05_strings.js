fbc5182de305c28207ea78ae3fda2fd3c84eee44
V1n2a3 <101231939+V1n2a3@users.noreply.github.com>
2025-07-30 11:15:19 +0000

    strings and numbers

---
+++ file1/05_strings.js
@@ -0,0 +1,42 @@
+const name ="vinay"
+const repocount = 50
+
+// console.log(`My name is ${name} and my repo count is ${repocount}`);
+
+const gameName = new String('tempest-wolf')
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+
+const newString = gameName.substring(0,6)
+
+// console.log(newString);
+
+const anotherString = gameName.slice(-12,9)
+// console.log(anotherString);
+
+const newStringOne = "    Vinay     "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://localhost.com/file%20transfer"
+// console.log(url.replace('%20','-'));
+// console.log(url.includes('file'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ file1/06_nums_and_maths.js
@@ -0,0 +1,21 @@
+const score = 400
+console.log(score);
+
+const balance = new Number((200))
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 25.677
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++ Maths ++++++++++++++++++ //
+
+
+
+
+
