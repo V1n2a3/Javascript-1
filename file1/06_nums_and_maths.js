14768bda8a5c30259dff212abf07ac960cefa461
V1n2a3 <101231939+V1n2a3@users.noreply.github.com>
2025-07-30 12:34:20 +0000

    numbers and maths

--- file1/06_nums_and_maths.js
+++ file1/06_nums_and_maths.js
@@ -1,20 +1,24 @@
 const score = 400
-console.log(score);
+// console.log(score);
 
 const balance = new Number((200))
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
 
 const otherNumber = 25.677
-console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(3));
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN'));
 
 // +++++++++++++++++++++ Maths ++++++++++++++++++ //
 
+const min = 10
+const max = 10
+
+console.log(Math.floor(Math.random() * (max - min + 10)) + min );
 
 
 
